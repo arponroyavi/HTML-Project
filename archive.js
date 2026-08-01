7639cf6375c963d2599b72223cdd67a431930a5a
arponroyavi <arponroy7667@gmail.com>
2026-08-01 10:38:18 +0600

    updated archive,js

--- archive.js
+++ archive.js
@@ -54,3 +54,10 @@ if (themeToggle) {
     themeLabel.textContent = themeToggle.checked ? "Dark Mode" : "Light Mode";
   });
 }
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('header nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
